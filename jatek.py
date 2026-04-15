34f2bd9358a15c815605ebaa436329b048cdab3c
mateszabi <mate.szabolcs.2024i@bankitatabanya.hu>
2026-04-15 10:21:23 +0200

    Update jatek.py

--- jatek.py
+++ jatek.py
@@ -15,17 +15,18 @@ class Karakter:
     def adatok(self):
         print(f"A jelenlegi Karaktered: {self.nev}\nÉletereje: {self.eletero}\nSebzése: {self.sebzes}\nképessége: {self.kepesseg}")
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> de003bf4d476f6c212548cef241a6448922f80fb
+
 karakter1 = Karakter("Harcos", 150, 30, "Nincs")
 karakter2 = Karakter("Varázsló", 120, 40, "Heal")
 karakter3 = Karakter("Íjász", 80, 45, "Nincs")
 
 karakterek = [karakter1,karakter2,karakter3]
 
+if bemenet == 1:
+    print(f"A játék elkezdődött!\n")
+    karakter = input(f"Válaszd ki a Karaktered az alábbiak közül:\n{karakter1.nev}\n{karakter2.nev}\n{karakter3.nev}\n")
 
 if karakter == karakter1.nev:
     karakter1.adatok()
@@ -33,4 +34,3 @@ elif karakter == karakter2.nev:
     karakter2.adatok()
 elif karakter == karakter3.nev:
     karakter3.adatok()
-
