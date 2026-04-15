d98942aaa41b372f77ff1f48cd0f65537a3a1a40
mateszabi <mate.szabolcs.2024i@bankitatabanya.hu>
2026-04-15 10:30:16 +0200

    Update jatek.py

--- jatek.py
+++ jatek.py
@@ -1,6 +1,8 @@
+print("---RPG Projekt---")
 print("1 = Kezdés")
 print("2 = Információk")
 print("3 = Kilépés")
+print("-----------------")
 
 bemenet = int(input("Válassz!(1-3): \n"))
 
@@ -24,6 +26,7 @@ karakter3 = Karakter("Íjász", 80, 45, "Nincs")
 
 karakterek = [karakter1,karakter2,karakter3]
 
+
 if bemenet == 1:
     print(f"A játék elkezdődött!\n")
     karakter = input(f"Válaszd ki a Karaktered az alábbiak közül:\n{karakter1.nev}\n{karakter2.nev}\n{karakter3.nev}\n")
@@ -34,3 +37,5 @@ elif karakter == karakter2.nev:
     karakter2.adatok()
 elif karakter == karakter3.nev:
     karakter3.adatok()
+
+
