cb7b3e96fb8fdd8f62e677ff1eddb2ce90ec5153
Levi1300 <osku.levente.balamber.2024i@bankitatabanya.hu>
2026-04-15 10:30:22 +0200

    Update jatek.py

--- jatek.py
+++ jatek.py
@@ -6,18 +6,19 @@ bemenet = int(input("Válassz!(1-3): \n"))
 
  
 class Karakter:
-    def __init__(self, nev, eletero, sebzes, kepesseg):
+    def __init__(self, nev, eletero, sebzes, kepesseg, penz):
         self.nev = nev
         self.eletero = eletero
         self.sebzes = sebzes
         self.kepesseg = kepesseg
+        self.penz = penz
 
     def adatok(self):
-        print(f"A jelenlegi Karaktered: {self.nev}\nÉletereje: {self.eletero}\nSebzése: {self.sebzes}\nképessége: {self.kepesseg}")
+        print(f"A jelenlegi Karaktered: {self.nev}\nÉletereje: {self.eletero}\nSebzése: {self.sebzes}\nképessége: {self.kepesseg}\nPénze: {self.penz}")
 
-karakter1 = Karakter("Harcos", 150, 30, "Nincs")
-karakter2 = Karakter("Varázsló", 120, 40, "Heal")
-karakter3 = Karakter("Íjász", 80, 45, "Nincs")
+karakter1 = Karakter("Harcos", 150, 30, "Nincs", 500)
+karakter2 = Karakter("Varázsló", 120, 40, "Heal", 450)
+karakter3 = Karakter("Íjász", 80, 45, "Nincs", 400)
 
 karakterek = [karakter1,karakter2,karakter3]
 
@@ -31,4 +32,6 @@ elif karakter == karakter2.nev:
     karakter2.adatok()
 elif karakter == karakter3.nev:
     karakter3.adatok()
+
+
 
