b0b98fc4b82b8585559351774eb1cb1e373ccf3c
mateszabi <mate.szabolcs.2024i@bankitatabanya.hu>
2026-04-22 09:11:57 +0200

    Update jatek.py

--- jatek.py
+++ jatek.py
@@ -1,8 +1,8 @@
 print("---RPG Projekt---")
-print("1 = Kezdés")
+print("1 = Játék")
 print("2 = Információk")
 print("3 = Kilépés")
-print("--------------1---")
+print("-----------------")
 
 bemenet = int(input("Válassz!(1-3): \n"))
 
@@ -18,24 +18,13 @@ class Karakter:
         print(f"A jelenlegi Karaktered: {self.nev}\nÉletereje: {self.eletero}\nSebzése: {self.sebzes}\nképessége: {self.kepesseg}")
 
 
-
-
-karakter1 = Karakter("Harcos", 150, 30, "Nincs")
-karakter2 = Karakter("Varázsló", 120, 40, "Heal")
-karakter3 = Karakter("Íjász", 80, 45, "Nincs")
-
-karakterek = [karakter1,karakter2,karakter3]
-
-
 if bemenet == 1:
     print(f"A játék elkezdődött!\n")
-    karakter = input(f"Válaszd ki a Karaktered az alábbiak közül:\n{karakter1.nev}\n{karakter2.nev}\n{karakter3.nev}\n")
+    karakter = str(input("Add meg a hősöd nevét!"))
 
-if karakter == karakter1.nev:
-    karakter1.adatok()
-elif karakter == karakter2.nev:
-    karakter2.adatok()
-elif karakter == karakter3.nev:
-    karakter3.adatok()
+
+karakter1 = Karakter(karakter, 150, 30, "Nincs")
+
+
 
 
