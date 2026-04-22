3d1ea54eba23bf44b3b19c9cdd016637119c7ae4
Levi1300 <osku.levente.balamber.2024i@bankitatabanya.hu>
2026-04-22 09:13:53 +0200

    Update jatek.py

--- jatek.py
+++ jatek.py
@@ -1,14 +1,13 @@
 print("---RPG Projekt---")
-print("1 = Játék")
+print("1 = Kezdés")
 print("2 = Információk")
 print("3 = Kilépés")
 print("-----------------\n")
 
 bemenet = int(input("Válassz!(1-3): \n"))
 
-<<<<<<< HEAD
 if bemenet == 2:
-    print("Információk")
+    print("---Információk---")
     print("infok")
     print("-----------------\n")
     print("---RPG Projekt---")
@@ -18,50 +17,28 @@ if bemenet == 2:
     print("-----------------")
     bemenet = int(input("Válassz!(1-3): \n"))
 
-=======
->>>>>>> b0b98fc4b82b8585559351774eb1cb1e373ccf3c
  
 class Karakter:
-    def __init__(self, nev, eletero, sebzes, kepesseg):
+    def __init__(self, nev, eletero, sebzes, kepesseg, penz):
         self.nev = nev
         self.eletero = eletero
         self.sebzes = sebzes
         self.kepesseg = kepesseg
+        self.penz = penz
 
     def adatok(self):
-<<<<<<< HEAD
         print(f"A jelenlegi Karaktered: {self.nev}\nÉletereje: {self.eletero}\nSebzése: {self.sebzes}\nképessége: {self.kepesseg}\nPénze: {self.penz}")
-
-karakter1 = Karakter("Harcos", 150, 30, "Nincs", 500)
-karakter2 = Karakter("Varázsló", 120, 40, "Heal", 450)
-karakter3 = Karakter("Íjász", 80, 45, "Nincs", 400)
-
-karakterek = [karakter1,karakter2,karakter3]
-=======
-        print(f"A jelenlegi Karaktered: {self.nev}\nÉletereje: {self.eletero}\nSebzése: {self.sebzes}\nképessége: {self.kepesseg}")
->>>>>>> b0b98fc4b82b8585559351774eb1cb1e373ccf3c
-
 
 if bemenet == 1:
     print(f"A játék elkezdődött!\n")
-<<<<<<< HEAD
-    karakter = input(f"Válaszd ki a Karaktered az alábbiak közül:\n{karakter1.nev}\n{karakter2.nev}\n{karakter3.nev}\n")
-
-if karakter == karakter1.nev:
-    karakter1.adatok()
-elif karakter == karakter2.nev:
-    karakter2.adatok()
-elif karakter == karakter3.nev:
-    karakter3.adatok()
+    karakter = input(f"")
 
 
-=======
-    karakter = str(input("Add meg a hősöd nevét!"))
+      
 
-
-karakter1 = Karakter(karakter, 150, 30, "Nincs")
+karakter1 = Karakter("Harcos", 150, 30, "Nincs", 500)
 
 
 
 
->>>>>>> b0b98fc4b82b8585559351774eb1cb1e373ccf3c
+
