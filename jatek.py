411d3793902ef48acd09adca83aa45a2df6f8a56
mateszabi <mate.szabolcs.2024i@bankitatabanya.hu>
2026-04-27 10:03:05 +0200

    Update jatek.py

--- jatek.py
+++ jatek.py
@@ -141,7 +141,7 @@ def harc(jatekos, ellenfel):
 
         if ellenfel.elet <= 0:
             print(f"\n✅ Legyőzted: {ellenfel.neve}!")
-            jatekos.penz += (randint(90, 300))
+            jatekos.penz += (randint(150, 300))
             jatekos.szint += 1
             return True
 
