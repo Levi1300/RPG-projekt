40204ea168052e2214c3a621b1a792d5cf8823b1
Levi1300 <osku.levente.balamber.2024i@bankitatabanya.hu>
2026-05-13 09:25:23 +0200

    Update jatek.py

--- jatek.py
+++ jatek.py
@@ -333,6 +333,9 @@ def jatek(jatekos):
                     jatekos.penz += 10000
                     legyozott += 1
 
+            elif uj_boss4 == legyozott:
+                print("Nyertél! 🏆")
+
             else:
                 ellenfel = uj_ellenfel(legyozott)
 
